3f2dc162dbbd3bce04865a3d5b6f091242bead46
Safei Ashraf <Safei.ashraf@gmail.com>
2020-09-23 06:45:44 +0200

    adding fetch movie logic on search

--- script/index.js
+++ script/index.js
@@ -1,1 +1,26 @@
-console.log('Hi there!');
+const apiLink = `http://www.omdbapi.com/?`;
+const apiKey = `ea52d5db`;
+//http://www.omdbapi.com/?t=fear&apikey=ea52d5db
+
+const fetchMovieData = async (searchString)=>{
+    const resp = await axios.get(apiLink,{
+        params:{
+            apikey: apiKey,
+            s: searchString
+        }
+    });
+    console.log(resp.data);  
+}
+const input = document.querySelector('input');
+let timeoutId;
+
+const onInput = (e)=>{
+    if(timeoutId){
+        clearTimeout(timeoutId);
+    }
+    timeoutId = setTimeout(()=>{fetchMovieData(e.target.value)},800);
+    
+    }
+
+input.addEventListener('input', onInput)
+
