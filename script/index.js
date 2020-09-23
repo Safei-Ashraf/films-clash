6acf47428974bdba467a5b3667a50eb25a6b05fc
Safei Ashraf <Safei.ashraf@gmail.com>
2020-09-23 08:58:01 +0200

    displaying search results

--- script/index.js
+++ script/index.js
@@ -16,7 +16,16 @@ const input = document.querySelector('input');
 
 const onInput = async (e)=>{
 const movies = await fetchMovieData(e.target.value);
-console.log(movies)
+for(let movie of movies)
+{
+    const div = document.createElement('div');
+    div.innerHTML = `
+    <img src="${movie.Poster}"/>
+    <h1>${movie.Title}</h1>`;
+    
+    document.querySelector('#target').appendChild(div);
+
+}
 }
 
 input.addEventListener('input', debouncer(onInput));
