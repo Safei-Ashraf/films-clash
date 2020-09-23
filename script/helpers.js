5854db42ead17ca4f80d65991c304461ce0eb283
Safei Ashraf <Safei.ashraf@gmail.com>
2020-09-23 07:13:39 +0200

    adding helper method into separate file

---
+++ script/helpers.js
@@ -0,0 +1,13 @@
+//delay api calls until user finishs typing: (debouncing an input>> waiting sometime after last event to do sth)
+const debouncer = (callback,delay = 800)=>{
+    let timeoutId;
+    return(...args)=>{
+        if(timeoutId){
+            clearTimeout(timeoutId);
+        }
+        timeoutId = setTimeout(()=>{ 
+            callback.apply(null,args)
+        },delay);
+    }
+}
+
--- script/index.js
+++ script/index.js
@@ -13,19 +13,7 @@ const fetchMovieData = async (searchString)=>{
 }
 const input = document.querySelector('input');
 
-const debouncer = (callback,delay = 800)=>{
-    let timeoutId;
-    return(...args)=>{
-        if(timeoutId){
-            clearTimeout(timeoutId);
-        }
-        timeoutId = setTimeout(()=>{ 
-            callback.apply(null,args)
-        },delay);
-    }
-}
 
-//delay api calls until user finishs typing: (debouncing an input>> waiting sometime after last event to do sth)
 const onInput = (e)=>{
 fetchMovieData(e.target.value);
 }
