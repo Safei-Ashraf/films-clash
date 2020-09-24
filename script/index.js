245c560e4c691e66a337dbf75798800f9eb067d9
Safei Ashraf <Safei.ashraf@gmail.com>
2020-09-24 04:32:35 +0200

    generating 2 lists

--- script/index.js
+++ script/index.js
@@ -1,85 +1,55 @@
 const apiLink = `http://www.omdbapi.com/?`;
 const apiKey = `ea52d5db`;
 //http://www.omdbapi.com/?t=fear&apikey=ea52d5db
-
-const fetchMovieData = async (searchString)=>{
-    const resp = await axios.get(apiLink,{
-        params:{
-            apikey: apiKey,
-            s: searchString
-        }
-    });
-    if(resp.data.Error){
-        return [];
-    }
-    return resp.data.Search  
-}
-
-//Autocomplete list Generation:
-const root = document.querySelector('.autocomplete');
-root.innerHTML = `
-<label><b>Search Films</b></label>
-<input class="input"/>
-<div class="dropdown">
-    <div class="dropdown-menu">
-        <div class="dropdown-content results">
-        </div>
-    </div>
-</div>
-`
-const results = document.querySelector('.results');
-const dropdown = document.querySelector('.dropdown');
-const input = document.querySelector('input');
-
-const onInput = async (e)=>{
-const movies = await fetchMovieData(e.target.value);
-clearIt(results);
-dropdown.classList.add('is-active');
-if(movies.length > 0){
-    for(let movie of movies)
-    {
-        const item = document.createElement('a');
+const autoCompleteConfig = {
+    renderOptions(movie){
         const imgSrc = movie.Poster === 'N/A'? '':movie.Poster;
-        item.classList.add('dropdown-item')
-        item.innerHTML = `
+        return  `
         <img src="${imgSrc}"/>
-        ${movie.Title}`;
-
-        item.addEventListener('click', ()=>{
-            dropdown.classList.remove('is-active');
-            input.value = movie.Title;
-            onMovieSelect(movie);
-        })
-
-        results.appendChild(item);
-     
+        ${movie.Title}  (${movie.Year})`
+    },
+    inputValue(movie){
+        return movie.Title;
+    },
+    async fetchData(searchString){
+            const resp = await axios.get(apiLink,{
+                params:{
+                    apikey: apiKey,
+                    s: searchString
+                }
+            });
+            if(resp.data.Error){
+                return [];
+            }
+            return resp.data.Search    
     }
-} else{
-    const item = document.createElement('a');
-    item.classList.add('dropdown-item')
-    item.innerHTML = `
-    No result Found`;
-    results.appendChild(item);
+
 }
+createAutocomplete({
+    ...autoCompleteConfig,
+    root: document.querySelector('#left-autocomplete'),
+    onOptionSelect(movie){
+        document.querySelector('.tutorial').classList.add('is-hidden');
+        onMovieSelect(movie,document.querySelector('#left-summary'));
     }
-
-input.addEventListener('input', debouncer(onInput));
+})
+createAutocomplete({
+    ...autoCompleteConfig,
+    root: document.querySelector('#right-autocomplete'),
+    onOptionSelect(movie){
+        document.querySelector('.tutorial').classList.add('is-hidden');
+        onMovieSelect(movie,document.querySelector('#right-summary'));
+    }
+})
 
 //2nd Request made to get full movie info from API:
-const onMovieSelect = async (movie)=>{
-   const resp = await axios.get(apiLink,{
+const onMovieSelect = async (movie,summaryElement)=>{
+    const resp = await axios.get(apiLink,{
     params:{
         apikey: apiKey,
         i: movie.imdbID
     }
-   });
-   document.querySelector('#summary').innerHTML = renderMovie(resp.data);
+    });
+    summaryElement.innerHTML = renderMovie(resp.data);
 }
-//Close dropdown automatically:
-document.addEventListener('click',e=>{
-    if(!root.contains(e.target))
-    {
-        dropdown.classList.remove('is-active');
-    }
-})
 
