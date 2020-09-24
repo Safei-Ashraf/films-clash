088f07c215633c67ffdb21792937f0debd056301
Safei Ashraf <Safei.ashraf@gmail.com>
2020-09-24 02:54:01 +0200

    adding movie awards info

--- script/helpers.js
+++ script/helpers.js
@@ -31,5 +31,25 @@ const renderMovie = (moviInfo)=>{
     <p>${moviInfo.Plot}</p>
     </div>
     </article>
+    <article class="notification is-warning">
+    <p class="title">${moviInfo.Awards}</p>
+    <p class="subtitle">Awards</p>
+    </article>
+    <article class="notification is-primary">
+    <p class="title">${moviInfo.imdbVotes}</p>
+    <p class="subtitle">IMDB Votes</p>
+    </article>
+    <article class="notification is-primary">
+    <p class="title">${moviInfo.BoxOffice}</p>
+    <p class="subtitle">Box Office</p>
+    </article>
+    <article class="notification is-primary">
+    <p class="title">${moviInfo.Metascore}</p>
+    <p class="subtitle">Metascore</p>
+    </article>
+    <article class="notification is-primary">
+    <p class="title">${moviInfo.imdbRating}</p>
+    <p class="subtitle">IMDB Rating</p>
+    </article>
     `
 }
